5fd889b8f03e2b1c6cf8eca469e03d729c744fba
timothy.bean <timothy.bean@NoraTech.com>
2018-01-31 00:06:21 -0500

    Turn on DEBUG

--- libloragw/inc/config.h
+++ libloragw/inc/config.h
@@ -2,9 +2,9 @@
 #define _LORAGW_CONFIGURATION_H
 	#define LIBLORAGW_VERSION	"5.0.1"
 	#define DEBUG_AUX	0
-	#define DEBUG_SPI	0
-	#define DEBUG_REG	0
-	#define DEBUG_HAL	0
+	#define DEBUG_SPI	1
+	#define DEBUG_REG	1
+	#define DEBUG_HAL	1
 	#define DEBUG_GPS	0
 	#define DEBUG_GPIO	
 	#define DEBUG_LBT	0
